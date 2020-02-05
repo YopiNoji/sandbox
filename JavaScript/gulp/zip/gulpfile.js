d3c14afeab4474b50e5f4fc42a2cfb4c1449a0c6
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-02-05 15:16:42 +0900

    up

--- JavaScript/gulp/zip/gulpfile.js
+++ JavaScript/gulp/zip/gulpfile.js
@@ -2,14 +2,6 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 const minimist = require('minimist');
 
-const config = {
-    region: 'ap-northeast-1',
-    handler: 'index.handler',
-    functionName: 'gulp',
-    role: 'arn:aws:iam::xxxxxxxxxxxx:role/service-role/test-gulp-xxxx', // 再デプロイ時は省略可能
-    runtime: 'nodejs12.x'
-  };
-
 const options = minimist(process.argv.slice(2), {
 	string: 'path',
 	default: {
