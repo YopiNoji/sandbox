66e3574f640e8a13c570146d9b09cfe625d2fce0
Masaki Yoshiiwa <yopinoji@yahoo.co.jp>
2020-02-18 19:41:17 +0900

    add new php snipet

---
+++ PHP/Snipet/dump_time_and_memory.php
@@ -0,0 +1,6 @@
+$startTime = microtime(true);
+$initialMemory = memory_get_usage();
+$runningTime =  microtime(true) - $startTime;
+$usedMemory = (memory_get_peak_usage() - $initialMemory) / (1024 * 1024);
+var_dump('running time: ' . $runningTime . ' [s]');
+var_dump('used memory: ' . $usedMemory . ' [MB]');
